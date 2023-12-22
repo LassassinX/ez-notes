7d5ac2adaae2a7e213bb4bae9af00ebde3770330
Sanjid Islam Chowdhury <56227702+LassassinX@users.noreply.github.com>
2023-12-22 11:54:06 +0600

    Update NavBar.tsx

--- src/components/templates/NavBar.tsx
+++ src/components/templates/NavBar.tsx
@@ -3,6 +3,9 @@ import Container from "../atoms/Container";
 import Logo from "../atoms/Logo";
 import { useSession, signIn, signOut } from "next-auth/react"
 
+// google icon from mui
+import GoogleIcon from '@mui/icons-material/Google';
+
 const NavBar = () => {
 	const { data: session, status } = useSession()
 
@@ -16,9 +19,16 @@ const NavBar = () => {
 				</button>
 			}{
 				!session &&
-				<button className="btn btn-outline btn-primary py-2 text-sm min-h-0 h-auto" onClick={() => { signIn() }}>
-					Sign in
-				</button>
+				<div className="dropdown dropdown-end">
+					<div tabIndex={0} role="button" className="btn btn-outline btn-primary py-2">Sign in</div>
+					<ul tabIndex={0} className="dropdown-content z-[1] menu shadow bg-base-100 rounded-box">
+						<li>
+							<button className="p-1">
+								<GoogleIcon className="text-primary" onClick={() => signIn('google')}/>
+							</button>
+						</li>
+					</ul>
+				</div>
 			}
 
 		</Container>
